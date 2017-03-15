3ac6827018b76e1a4f7b3debf2958b26bbb7aafd
FrancesDevanbu <frances.devanbu@students.olin.edu>
2017-03-14 20:28:23 -0400

    Updating my Flask toolbox turn in

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,24 @@
-"""
-Put your Flask app code here.
-"""
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        name=request.form['name']
+        age=request.form['age']
+        ninja=request.form['ninja']
+        if not (name =='Name' and age =='Age' and ninja=='Ninja'):
+            return render_template('profile.html', name=name, age=age)
+
+        else:
+            return render_template('error.html')
+
+if __name__ == '__main__':
+    app.run()
--- hello.py
+++ hello.py
@@ -1,13 +1,24 @@
-"""
-Simple "Hello, World" application using Flask
-"""
-
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index Page'
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello World
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    if not name:
+        return 'index page'
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
