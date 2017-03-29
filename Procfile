3a6f61d757fc1a47049191762173f40cbc5cf686
FrancesDevanbu <frances.devanbu@students.olin.edu>
2017-03-28 20:50:23 -0400

    changing host and port

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
- web: python3 hello.py
- 
+web: python3 flask_app.py
--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os.environ
+
+
+HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+PORT = int(os.environ.get('PORT', 5000)))
+app.run(host=HOST, port=PORT)
 
 app = Flask(__name__)
 
